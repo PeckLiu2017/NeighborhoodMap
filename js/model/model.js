03431a712807558133044e41dd8a7784fb33d361
peck liu <xiaoyang20170204@gmail.com>
2017-11-24 19:22:19 +0800

    docs:add js comments in model.js

--- js/model/model.js
+++ js/model/model.js
@@ -1,6 +1,11 @@
+/**
+ *  @description this file store locations and map style datas as well as create Class Location
+ */
 
 
-// Create default locations to be listed in places list
+/**
+ *  @description location datas form Udacity course
+ */
 var locations = [{
     title: 'Park Ave Penthouse',
     location: {
@@ -45,119 +50,181 @@ var locations = [{
   }
 ];
 
-// Create a new StyledMapType object, passing it an array of styles
-var styles = [
-      {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
-      {
-        featureType: 'administrative',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#c9b2a6'}]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#dcd2be'}]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#ae9e90'}]
-      },
-      {
-        featureType: 'landscape.natural',
-        elementType: 'geometry',
-        stylers: [{color: '#dfd2ae'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{color: '#dfd2ae'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#93817c'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry.fill',
-        stylers: [{color: '#a5b076'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#447530'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#f5f1e6'}]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'geometry',
-        stylers: [{color: '#fdfcf8'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#f8c967'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#e9bc62'}]
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry',
-        stylers: [{color: '#e98d58'}]
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#db8555'}]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#806b63'}]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{color: '#dfd2ae'}]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#8f7d77'}]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#ebe3cd'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{color: '#dfd2ae'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry.fill',
-        stylers: [{color: '#b9d3c2'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#92998d'}]
-      }
-    ];
+/**
+ *  @description style datas form Udacity course
+ *  it will be used to create a new StyledMapType object that passing it as an array of styles
+ */
+var styles = [{
+    elementType: 'geometry',
+    stylers: [{
+      color: '#ebe3cd'
+    }]
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#523735'
+    }]
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [{
+      color: '#f5f1e6'
+    }]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [{
+      color: '#c9b2a6'
+    }]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry.stroke',
+    stylers: [{
+      color: '#dcd2be'
+    }]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#ae9e90'
+    }]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#dfd2ae'
+    }]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#dfd2ae'
+    }]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#93817c'
+    }]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [{
+      color: '#a5b076'
+    }]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#447530'
+    }]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#f5f1e6'
+    }]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#fdfcf8'
+    }]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#f8c967'
+    }]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{
+      color: '#e9bc62'
+    }]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#e98d58'
+    }]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry.stroke',
+    stylers: [{
+      color: '#db8555'
+    }]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#806b63'
+    }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#dfd2ae'
+    }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#8f7d77'
+    }]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.stroke',
+    stylers: [{
+      color: '#ebe3cd'
+    }]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [{
+      color: '#dfd2ae'
+    }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [{
+      color: '#b9d3c2'
+    }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{
+      color: '#92998d'
+    }]
+  }
+];
 
-let Location = function (data) {
+/**
+ *  @description Use konckout to manage Class Location
+ */
+let Location = function(data) {
   this.title = data.title;
   this.location = data.location;
 }
